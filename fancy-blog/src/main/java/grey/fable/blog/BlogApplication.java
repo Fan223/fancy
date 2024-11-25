7a870e2cf15ef238a8574d1a5c937f9ff8b32022
GreyFable <fanj.zh@foxmail.com>
2024-11-25 09:27:06 +0800

    init

---
+++ fancy-blog/src/main/java/grey/fable/blog/BlogApplication.java
@@ -0,0 +1,17 @@
+package grey.fable.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 博客系统启动类.
+ *
+ * @author GreyFable
+ * @since 2024/11/25 4:14
+ */
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
---
+++ fancy-vocabulary/src/main/java/grey/fable/vocabulary/VocabularyApplication.java
@@ -0,0 +1,17 @@
+package grey.fable.vocabulary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 词汇系统启动类.
+ *
+ * @author GreyFable
+ * @since 2024/11/25 4:15
+ */
+@SpringBootApplication
+public class VocabularyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VocabularyApplication.class, args);
+    }
+}
