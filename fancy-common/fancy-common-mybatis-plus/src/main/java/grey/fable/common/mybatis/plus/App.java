0694068fd2baaa4edc491bf017a0902b2b980ed9
Fan <fanj.zh@foxmail.com>
2025-03-06 06:44:54 +0800

    Init.

---
+++ fancy-common/fancy-common-mybatis-plus/src/main/java/grey/fable/common/mybatis/plus/App.java
@@ -0,0 +1,13 @@
+package grey.fable.common.mybatis.plus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ fancy-services/fancy-blog/src/main/java/grey/fable/blog/App.java
@@ -0,0 +1,13 @@
+package grey.fable.blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
